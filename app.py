3393c865208a6d813b99e38d6461fae0c930dbdd
eLafo <javierlafora@gmail.com>
2023-06-05 21:15:46 +0200

    reads pdf

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from dotenv import load_dotenv
-import os
+import streamlit as st
+from PyPDF2 import PdfReader
 
 def main():
     load_dotenv()
-    print(os.getenv('OPENAI_API_KEY'))
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+
+    # Upload PDF
+    pdf = st.file_uploader("Upload your PDF", type=["pdf"])
+    
+    # Extract text from PDF
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        st.write(text)
 
 if __name__ == '__main__':
     main()
