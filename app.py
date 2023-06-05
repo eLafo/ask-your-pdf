e7aaa4a5338880056b3cdabebaf17cca56ab3011
eLafo <javierlafora@gmail.com>
2023-06-05 21:33:53 +0200

    creates embeddings

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 def main():
     load_dotenv()
@@ -28,6 +30,9 @@ def main():
 
         chunks = text_splitter.split_text(text)
 
-        st.write(chunks)
+        # Create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        st.write(knowledge_base)
 if __name__ == '__main__':
     main()
